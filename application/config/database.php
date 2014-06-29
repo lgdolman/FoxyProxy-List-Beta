0e8b00287794d4e6a80561cac4f134f35aecc047
lgdolman <l.g.dolman@gmail.com>
2014-06-29 21:21:38 +0100

    Added Database conenctions and other basic CI Settings
    Signed-off-by: lgdolman <l.g.dolman@gmail.com>

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'codeigniter';
+$db['default']['password'] = 'codeigniter';
+$db['default']['database'] = 'codeigniter';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
